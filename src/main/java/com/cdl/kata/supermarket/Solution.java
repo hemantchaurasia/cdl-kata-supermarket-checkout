a3e1cd51c9624609a557c750e9cde3d905ff5578
hemantchaurasia <hem.kum@gmail.com>
2023-04-10 21:33:40 +0100

    Unit test cases

--- src/main/java/com/cdl/kata/supermarket/Solution.java
+++ src/main/java/com/cdl/kata/supermarket/Solution.java
@@ -1,7 +1,5 @@
 package com.cdl.kata.supermarket;
 
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -9,150 +7,61 @@ import java.util.Scanner;
  */
 class Solution {
 
-    /*
-    The solution should allow for items to input at the command line,
-    and allow for a final total to be calculated and for a running total after each item is ‘scanned’.
-     */
-    private Scanner scanner;
-    private PrintStream printStream;
+    static CheckoutItems ck = new CheckoutItems();
 
-    public Solution(InputStream inputStream, PrintStream printStream) {
-        this.scanner = new Scanner(inputStream);
-        this.printStream = printStream;
-    }
-
-    public void start() {
-        Scanner sc = new Scanner(System.in);
-        int exitInput = sc.nextInt();
+    public static void start(int exitInput) {
         do {
-            printStream.println("Enter the item: ");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the item: ");
             String item = scanner.nextLine();
-            printStream.println("Enter the quantity");
+            System.out.println("Enter the quantity");
             int quantity = scanner.nextInt();
-            printStream.println("Item: " + item );
-            printStream.println("Quantity: " + quantity );
-            printStream.println("Do you want to continue your shopping. Or press 0 to finish!");
-        }while(exitInput != 0);
+
+            System.out.println(" ---------------------------- :");
+            if ((ItemPriceDetails.itemPriceMap.get(item) == null)) {
+                System.out.println("Item is not available in store.");
+            } else {
+                    System.out.println("");
+                    ck.addItem(item, quantity);
+                    try {
+                        ck.reviewCart();
+                    } catch (Exception e) {
+                        System.out.println(e.toString());
+                    }
+                    Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
+                    System.out.println(" ---------------------------- :");
+                    System.out.println("Total Price: " + totalPrice);
+                }
+            Scanner sc = new Scanner(System.in);
+            System.out.println("To continue shopping enter 1 OR enter 0 to exit ...");
+            int exitInput2 = sc.nextInt();
+            exitInput = exitInput2;
+        } while (exitInput != 0);
     }
 
     public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println(" ================ Available Items in store ================");
+        System.out.println(" Items Name   prices :");
+        System.out.println("   A            50p");
+        System.out.println("   B            30p");
+        System.out.println("   C            20p");
+        System.out.println("   D             15");
+        System.out.println(" ");
+        System.out.println(" ================ Offers on Items ==============");
+        System.out.println(" A   3 for 130p");
+        System.out.println(" B   2 for 45p");
+        System.out.println(" ");
+        System.out.println(" ---------------------------------------");
+        System.out.println(" ");
 
-        System.out.println(" Items and prices :");
-        System.out.println(" A - 50 ");
-        System.out.println(" B - 30");
-        System.out.println(" C - 20");
-        System.out.println(" D -  15");
-
-        System.out.println(" Offers :");
-        System.out.println(" A - 3 = 1.30");
-        System.out.println(" B - 2 = .45");
-
-        Solution solution = new Solution(System.in, System.out);
-        solution.start();
-    }
-
-    public String run() {
-        CheckoutItems ck = new CheckoutItems();
-
-        // When the cart is empty check the contents
-        // This should throw an exception because the cart is empty
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-
-        // Here you add 3 quantities of item A
-        // and then print the total price and also review the cart
-        System.out.println("");
-        ck.addItem("A", 3);
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice);
-
-        // Here you add 2 quantities of item B to the existing cart
-        // and then print the total price and also review the cart
-        System.out.println("");
-        ck.addItem("B", 2);
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice2 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice2);
-
-        // Here you add 4 quantities of item C to the existing cart
-        // and then print the total price and also review the cart
-        System.out.println("");
-        ck.addItem("C", 4);
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice3 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice3);
-
-        // Here you remove 2 quantities of item B from the existing cart
-        // and then print the total price and also review the cart
-        System.out.println("");
-        try {
-            ck.removeItem("B", 2);
-        } catch (Exception e1) {
-            System.out.println(e1.toString());
-        }
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice4 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice4);
-
-        // Here you remove 1 quantity of item A from the existing cart
-        // and then print the total price and also review the cart
-        System.out.println("");
-        try {
-            ck.removeItem("A", 1);
-        } catch (Exception e1) {
-            System.out.println(e1.toString());
-        }
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice5 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice5);
-
-        // Here you empty your cart and then review
-        // the total price and content of the cart
-        System.out.println("");
-        ck.emptyCart();
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice6 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice6);
-
-        // Add item A again with quantity of 4 and review the price and content of the cart
-        System.out.println("");
-        ck.addItem("A", 4);
-        try {
-            ck.reviewCart();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        Double totalPrice7 = ck.calculateTotalPrice(ck.itemsListMap);
-        System.out.println("Total Price: " + totalPrice7);
-        return "Cart has been processed successfully!";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press 1 to start shopping");
+        System.out.println("Press 0 to quit");
+        int exitInput = sc.nextInt();
+        start(exitInput);
     }
 }
+
+
 
--- src/test/java/com/cdl/kata/supermarket/CheckoutItemsTest.java
+++ src/test/java/com/cdl/kata/supermarket/CheckoutItemsTest.java
@@ -16,11 +16,56 @@ public class CheckoutItemsTest {
     }
 
     @Test
-    void testExpectedException() {
+    @DisplayName("When Cart is empty")
+    void whenCatIsEmpty() {
         Exception thrown = Assertions.assertThrows(Exception.class, () -> {
             checkoutItems.reviewCart();
         }, "Your cart is empty");
         Assertions.assertEquals("Your cart is empty", thrown.getMessage());
     }
 
+    @Test
+    @DisplayName("Test offer When A of 3 quantity")
+    void testOfferAof3Qty() {
+        CheckoutItems ck = new CheckoutItems();
+        ck.addItem("A", 3);
+        Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
+        Assertions.assertEquals(130, totalPrice);
+    }
+
+    @Test
+    @DisplayName("Test offer When B of 2 quantity")
+    void testOfferBof2Qty() {
+        CheckoutItems ck = new CheckoutItems();
+        ck.addItem("B", 2);
+        Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
+        Assertions.assertEquals(45, totalPrice);
+    }
+
+    @Test
+    @DisplayName("Test price by removing some quantity from Cart")
+    void testTotalPriceRemovingItemFromCart() throws Exception {
+        CheckoutItems ck = new CheckoutItems();
+        ck.addItem("A", 3);
+        ck.addItem("B", 2);
+        ck.addItem("D", 1);
+        //Remove item B
+        ck.removeItem("B", 2);
+        Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
+        Assertions.assertEquals(145, totalPrice);
+    }
+
+    @Test
+    @DisplayName("Test emptying your cart and then review the price")
+    void testEmptyCartPrice() throws Exception {
+        CheckoutItems ck = new CheckoutItems();
+        ck.addItem("A", 3);
+        ck.addItem("B", 2);
+        ck.addItem("C", 2);
+        ck.addItem("D", 3);
+        //Remove item B
+        ck.emptyCart();
+        Double totalPrice = ck.calculateTotalPrice(ck.itemsListMap);
+        Assertions.assertEquals(0.0, totalPrice);
+    }
 }
